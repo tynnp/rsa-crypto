aea4d7a865d9990ea07d9f31385ee43b7abda770
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2025-03-12 03:38:32 +0700

    Chuyển lưu txt sang json

--- RSA_encrypt.py
+++ RSA_encrypt.py
@@ -1,50 +1,29 @@
 from RSA_algorithm import *
 
-input_file = input("Nhập đường dẫn file cần mã hóa: ").strip()
-base, ext = os.path.splitext(input_file)
-
-with open(input_file, 'rb') as f:
-    plaintext = f.read()
-    
-plaintext_file = "plaintext.txt"
-with open(plaintext_file, 'w') as f:
-    f.write(" ".join(map(str, plaintext)))
-print("Bản rõ đã được lưu tại:", plaintext_file)
-    
-# Sinh khóa RSA:
-p, q, n, phi, e, d = generate_keys(min_prime = 10000000, max_prime = 70000000)
-
+# Lấy nội dung file cần mã hóa
 print("-" * 100)
-print("Thông tin mã hóa:")
-print(f"p = {p}")
-print(f"q = {q}")
-print(f"n = {n}")
-print(f"phi(n) = {phi}")
-print(f"e = {e}")
-print(f"d = {d}")
-print(f"-> Khóa công khai: {e, n}")
-print(f"-> Khóa bí mật: {d, n}")
-    
-# Lưu thông tin khóa và định dạng file
-info = {
-    "p": p, 
-    "q": q,
-    "n": n, 
-    "phi": phi,
-    "e": e,
-    "d": d, 
-    "ext": ext
+file_path = input("Nhập đường dẫn đến file cần mã hóa: ")
+with open(file_path, "rb") as f:
+    file_bytes = f.read()
+plaintext_int = list(file_bytes)
+
+# Lấy nội dung khóa công khai
+pub_file = "public_key.json"
+with open(pub_file, "r") as f:
+    public_key = json.load(f)
+
+e = public_key["e"]
+n = public_key["n"]
+
+# Thực hiện mã hóa và tạo thông điệp
+ciphertext = encrypt(plaintext_int, e, n)
+message = {
+    "filename": os.path.basename(file_path),
+    "ciphertext": ciphertext
 }
 
-with open("info.txt", 'w') as f:
-    for k, v in info.items():
-        f.write(f"{k}:{v}\n")
-print("Thông tin khóa và định dạng file được lưu tại info.txt")
-
-# Mã hóa và lưu lại dưới dàn file
-ciphertext = encrypt(plaintext, e, n)
-ciphertext_file = "ciphertext.txt"
-with open(ciphertext_file, 'w') as f:
-    f.write(" ".join(map(str, ciphertext)))
-print(f"Bản mã đã được lưu tại: {ciphertext_file}")
-print("-" * 100)
+# Ghi lại thông điệp đã được mã hóa
+message_file = "message.json"
+with open(message_file, "w") as f:
+    json.dump(message, f, separators=(',', ': '))
+print(f"file thông điệp được lưu tại {message_file}", "-" * 100, sep='\n')
